7010e3d48efafb1fd12a4a29e1d6cf6e86396d8b
Krzysztof Piatek <krzych1p2@wp.pl>
2018-11-19 19:52:35 +0100

    halo

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+let nav = '<div>\n' +
+    '<nav>\n' +
+    '    <ul>\n' +
+    '        <li>\n' +
+    '             <li><a href="index.html">Strona główna</a></li>\n'+
+    '             <li><a href="contact.html">Kontakt</a></li>\n'+
+    '             <li><a href="map.html">Mapa</a></li>\n'+
+    '        </li>\\n\' +\n' +
+    '    </ul>\\n\' +\n' +
+    '\n' +
+    '</nav>\n +'
+    '</div>';
+
+let navbar = document.querySelector(' .navbar');
+
+navbar.innerHTML = nav;
